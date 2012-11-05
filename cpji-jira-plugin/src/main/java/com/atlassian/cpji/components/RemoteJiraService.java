9f50e0bea336a36371a828c836844282bfb91235
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-11-05 15:24:41 +0100

    JRADEV-16061: changed RemoteProjectService to RemoteJiraService, extending its scope, added serverInfo call so we can check the wire

--- cpji-jira-plugin/src/main/java/com/atlassian/cpji/components/RemoteJiraService.java
+++ cpji-jira-plugin/src/main/java/com/atlassian/cpji/components/RemoteJiraService.java
@@ -8,7 +8,9 @@ import com.atlassian.applinks.api.CredentialsRequiredException;
 import com.atlassian.applinks.api.application.jira.JiraApplicationType;
 import com.atlassian.fugue.Either;
 import com.atlassian.jira.rest.client.domain.BasicProject;
+import com.atlassian.jira.rest.client.domain.ServerInfo;
 import com.atlassian.jira.rest.client.internal.json.BasicProjectsJsonParser;
+import com.atlassian.jira.rest.client.internal.json.ServerInfoJsonParser;
 import com.atlassian.sal.api.net.Request;
 import com.atlassian.sal.api.net.Response;
 import com.atlassian.sal.api.net.ResponseException;
@@ -16,6 +18,7 @@ import com.google.common.collect.Maps;
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.codehaus.jettison.json.JSONTokener;
 
 import javax.annotation.Nonnull;
@@ -31,10 +34,28 @@ public class RemoteJiraService {
 		this.applicationLinkService = applicationLinkService;
 	}
 
+	@Nonnull
+	public Map<ApplicationLink, Either<ResponseStatus, ServerInfo>> getServers() {
+		final Map<ApplicationLink, Either<ResponseStatus, ServerInfo>> result = Maps.newHashMap();
+		for (ApplicationLink jira : applicationLinkService.getApplicationLinks(JiraApplicationType.class)) {
+			result.put(jira, getServerInfo(jira));
+		}
+		return result;
+	}
+
+	private Either<ResponseStatus, ServerInfo> getServerInfo(ApplicationLink jiraServer) {
+		return callRestService(jiraServer, "/rest/api/latest/serverInfo", new AbstractJsonResponseHandler<ServerInfo>() {
+			@Override
+			protected ServerInfo parseResponse(Response response) throws ResponseException, JSONException {
+				return new ServerInfoJsonParser().parse(new JSONObject(new JSONTokener(response.getResponseBodyAsString())));
+			}
+		});
+	}
+
 	// @todo should be multi-threaded
 	@Nonnull
 	public Map<ApplicationLink, Either<ResponseStatus, Iterable<BasicProject>>> getProjects() {
-		Map<ApplicationLink, Either<ResponseStatus, Iterable<BasicProject>>> result = Maps.newHashMap();
+		final Map<ApplicationLink, Either<ResponseStatus, Iterable<BasicProject>>> result = Maps.newHashMap();
 		for (ApplicationLink jira : applicationLinkService.getApplicationLinks(JiraApplicationType.class)) {
 			result.put(jira, getProjects(jira));
 		}
@@ -43,33 +64,19 @@ public class RemoteJiraService {
 
 	@Nonnull
 	public Either<ResponseStatus, Iterable<BasicProject>> getProjects(ApplicationLink jiraServer) {
-		try {
-			ApplicationLinkRequest request = jiraServer.createAuthenticatedRequestFactory().createRequest(Request.MethodType.GET, "/rest/api/latest/project");
-			return request.execute(new ApplicationLinkResponseHandler<Either<ResponseStatus, Iterable<BasicProject>>>()
-			{
-				public Either<ResponseStatus, Iterable<BasicProject>> credentialsRequired(final Response response) throws ResponseException
-				{
-					return Either.left(ResponseStatus.AUTHORIZATION_REQUIRED);
-				}
+		return callRestService(jiraServer, "/rest/api/latest/project", new AbstractJsonResponseHandler<Iterable<BasicProject>>() {
+			@Override
+			protected Iterable<BasicProject> parseResponse(Response response) throws ResponseException, JSONException {
+				return new BasicProjectsJsonParser().parse(
+						new JSONArray(new JSONTokener(response.getResponseBodyAsString())));
+			}
+		});
+	}
 
-				public Either<ResponseStatus, Iterable<BasicProject>> handle(final Response response) throws ResponseException
-				{
-					if (log.isDebugEnabled())
-					{
-						log.debug("Response is: " + response.getResponseBodyAsString());
-					}
-					if (response.getStatusCode() == 401)
-					{
-						return Either.left(ResponseStatus.AUTHENTICATION_FAILED);
-					}
-					try {
-						return Either.right(new BasicProjectsJsonParser().parse(new JSONArray(new JSONTokener(response.getResponseBodyAsString()))));
-					} catch (JSONException e) {
-						log.error("Failed to parse JSON", e);
-						return Either.left(ResponseStatus.COMMUNICATION_FAILED);
-					}
-				}
-			});
+	private <T> Either<ResponseStatus, T> callRestService(final ApplicationLink jiraServer, final String path, final AbstractJsonResponseHandler handler) {
+		try {
+			ApplicationLinkRequest request = jiraServer.createAuthenticatedRequestFactory().createRequest(Request.MethodType.GET, path);
+			return (Either<ResponseStatus, T>) request.execute(handler);
 		}
 		catch (CredentialsRequiredException ex)
 		{
@@ -78,6 +85,34 @@ public class RemoteJiraService {
 			log.error("Failed to transform response", e);
 			return Either.left(ResponseStatus.COMMUNICATION_FAILED);
 		}
+	}
+
+	protected static abstract class AbstractJsonResponseHandler<T> implements ApplicationLinkResponseHandler<Either<ResponseStatus, T>>
+	{
+		public Either<ResponseStatus, T> credentialsRequired(final Response response) throws ResponseException
+		{
+			return Either.left(ResponseStatus.AUTHORIZATION_REQUIRED);
+		}
+
+		public Either<ResponseStatus, T> handle(final Response response) throws ResponseException
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("Response is: " + response.getResponseBodyAsString());
+			}
+			if (response.getStatusCode() == 401)
+			{
+				return Either.left(ResponseStatus.AUTHENTICATION_FAILED);
+			}
+			try {
+				return Either.right(parseResponse(response));
+			} catch (JSONException e) {
+				log.error("Failed to parse JSON", e);
+				return Either.left(ResponseStatus.COMMUNICATION_FAILED);
+			}
+		}
+
+		protected abstract T parseResponse(Response response) throws ResponseException, JSONException;
 	}
 
 }
