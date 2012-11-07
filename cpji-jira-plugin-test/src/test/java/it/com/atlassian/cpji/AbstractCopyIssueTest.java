e88717c0e480c7e54de617ba02af105a8686ebbe
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-11-07 14:36:07 +0100

    changed integration tests to report more details on failure

--- cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/AbstractCopyIssueTest.java
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/AbstractCopyIssueTest.java
@@ -1,8 +1,11 @@
 package it.com.atlassian.cpji;
 
 import com.atlassian.jira.pageobjects.JiraTestedProduct;
+import com.atlassian.jira.webtest.webdriver.setup.JiraWebTestRules;
 import com.atlassian.pageobjects.DefaultProductInstance;
 import com.atlassian.pageobjects.TestedProductFactory;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
 
 /**
  * @since v2.1
@@ -11,4 +14,9 @@ public abstract class AbstractCopyIssueTest
 {
     static JiraTestedProduct jira1 = TestedProductFactory.create(JiraTestedProduct.class, new DefaultProductInstance("http://localhost:2990/jira", "jira1", 2990, "/jira"), null);
     static JiraTestedProduct jira2 = TestedProductFactory.create(JiraTestedProduct.class, new DefaultProductInstance("http://localhost:2991/jira", "jira2", 2991, "/jira"), null);
+
+	@Rule
+	public TestRule webTestRule1 = JiraWebTestRules.forJira(jira1);
+	@Rule
+	public TestRule webTestRule2 = JiraWebTestRules.forJira(jira2);
 }
