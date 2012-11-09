7f1d583857f054cc5eae9a9f877612024e66ef8f
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-11-09 15:51:43 +0100

    JRADEV-16334: tests enable PDL and don't disable it

--- cpji-jira-plugin-test/src/main/java/com.atlassian.cpji.tests.rules/EnableDarkFeatureRule.java
+++ cpji-jira-plugin-test/src/main/java/com.atlassian.cpji.tests.rules/EnableDarkFeatureRule.java
@@ -9,10 +9,16 @@ import javax.annotation.Nonnull;
 public class EnableDarkFeatureRule extends ExternalResource {
 	private final ImmutableList<JiraTestedProduct> jiras;
 	private final String feature;
+	private boolean disableAfter = false;
 
 	public <T extends JiraTestedProduct> EnableDarkFeatureRule(@Nonnull String s, T...jiras) {
 		this.feature = s;
 		this.jiras = ImmutableList.<JiraTestedProduct>copyOf(jiras);
+	}
+
+	public EnableDarkFeatureRule andDisableAfter() {
+		this.disableAfter = true;
+		return this;
 	}
 
 	@Override
@@ -25,8 +31,10 @@ public class EnableDarkFeatureRule extends ExternalResource {
 
 	@Override
 	protected void after() {
-		for(JiraTestedProduct jira : jiras) {
-			jira.backdoor().darkFeatures().disableForSite(feature);
+		if (this.disableAfter) {
+			for(JiraTestedProduct jira : jiras) {
+				jira.backdoor().darkFeatures().disableForSite(feature);
+			}
 		}
 		super.after();
 	}
