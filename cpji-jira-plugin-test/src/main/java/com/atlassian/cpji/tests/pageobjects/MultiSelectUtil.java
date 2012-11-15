678de9f081d40bd8b9c96a14471c497e1a948dce
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-11-15 11:44:07 +0100

    NONE: fixed TestCopyIssue.java to actually set the project using SingleSelect, copied SingleSelect page object to our source code so we can use it for all JIRA versions

---
+++ cpji-jira-plugin-test/src/main/java/com/atlassian/cpji/tests/pageobjects/MultiSelectUtil.java
@@ -0,0 +1,71 @@
+package com.atlassian.cpji.tests.pageobjects;
+
+import com.atlassian.pageobjects.PageBinder;
+import com.google.common.collect.Lists;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ *
+ * @since v2.1
+ */
+public class MultiSelectUtil {
+	public static void setMultiSelect(@Nonnull PageBinder pageBinder, @Nonnull String id, @Nullable Iterable<String> values) {
+		final Context cx = Context.enter();
+		try {
+			final Scriptable scope = cx.initStandardObjects();
+			final List<String> items = Lists.newArrayList();
+			scope.put("items", scope, items);
+			scope.put("select", scope, pageBinder.bind(getMultiSelectClass(pageBinder), id));
+
+			cx.evaluateString(scope, "if(select.clear) { select.clear(); } if (select.clearAllItems) { select.clearAllItems(); }", "js", 1, null);
+			if (values != null) {
+				for (String value : values) {
+					cx.evaluateString(scope, "select.add('" + value + "');", "js", 1, null);
+				}
+			}
+		} finally {
+			cx.exit();
+		}
+	}
+
+	public static Iterable<String> getMultiSelect(@Nonnull PageBinder pageBinder, @Nonnull String id) {
+		final Context cx = Context.enter();
+		try {
+			final Scriptable scope = cx.initStandardObjects();
+			final List<String> items = Lists.newArrayList();
+			scope.put("items", scope, items);
+			scope.put("select", scope, pageBinder.bind(getMultiSelectClass(pageBinder), id));
+
+			cx.evaluateString(scope, "var selected = select.getItems();"
+					+ "if (selected.byDefaultTimeout) {"
+					+ "selected = selected.byDefaultTimeout().iterator();"
+					+ "while(selected.hasNext()) { items.add(selected.next().getName()); }"
+					+ "} else {"
+					+ "for(var s = selected.size(), i = 0; i<s; ++i) { items.add(selected.get(i).getName()); }"
+					+ "}", "js", 1, null);
+
+			return items;
+		} finally {
+			cx.exit();
+		}
+	}
+
+	protected static <T> Class getMultiSelectClass(T owner) {
+		Class select;
+		try {
+			select = owner.getClass().getClassLoader().loadClass("com.atlassian.jira.pageobjects.components.MultiSelect");
+		} catch (ClassNotFoundException e) {
+			try {
+				select = owner.getClass().getClassLoader().loadClass("com.atlassian.jira.pageobjects.components.fields.MultiSelect");
+			} catch (ClassNotFoundException e1) {
+				throw new RuntimeException(e1);
+			}
+		}
+		return select;
+	}
+}
--- cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/pages/ConfigureCopyIssuesAdminActionPage.java
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/pages/ConfigureCopyIssuesAdminActionPage.java
@@ -1,10 +1,10 @@
 package it.com.atlassian.cpji.pages;
 
+import com.atlassian.cpji.tests.pageobjects.MultiSelectUtil;
 import com.atlassian.jira.pageobjects.JiraTestedProduct;
 import com.atlassian.jira.pageobjects.pages.AbstractJiraPage;
 import com.atlassian.jira.pageobjects.project.summary.ProjectSummaryPageTab;
 import com.atlassian.jira.pageobjects.project.summary.SettingsPanel;
-import com.atlassian.pageobjects.PageBinder;
 import com.atlassian.pageobjects.elements.ElementBy;
 import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.query.TimedCondition;
@@ -12,9 +12,6 @@ import com.atlassian.pageobjects.elements.query.webdriver.WebDriverQueryFunction
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
 import org.openqa.selenium.By;
 
 import javax.annotation.Nonnull;
@@ -40,61 +37,13 @@ public class ConfigureCopyIssuesAdminActionPage extends AbstractJiraPage {
     }
 
 	public ConfigureCopyIssuesAdminActionPage setAllowedGroups(@Nullable Iterable<String> groups) {
-		setMultiSelect(this.pageBinder, "groups", groups);
+		MultiSelectUtil.setMultiSelect(this.pageBinder, "groups", groups);
 		return this;
-	}
-
-	protected static void setMultiSelect(@Nonnull PageBinder pageBinder, @Nonnull String id, @Nullable Iterable<String> values) {
-		final Context cx = Context.enter();
-		try {
-			final Scriptable scope = cx.initStandardObjects();
-			final List<String> items = Lists.newArrayList();
-			scope.put("items", scope, items);
-			scope.put("select", scope, pageBinder.bind(getMultiSelectClass(pageBinder), id));
-
-			cx.evaluateString(scope, "select.clear();", "js", 1, null);
-			if (values != null) {
-				for (String value : values) {
-					cx.evaluateString(scope, "select.add('" + value + "');", "js", 1, null);
-				}
-			}
-		} finally {
-			cx.exit();
-		}
 	}
 
 	@Nonnull
 	public Iterable<String> getAllowedGroups() {
-		return getMultiSelect(this.pageBinder, "groups");
-	}
-
-	protected static Iterable<String> getMultiSelect(@Nonnull PageBinder pageBinder, @Nonnull String id) {
-		final Context cx = Context.enter();
-		try {
-//		return Iterables.transform(this.groups.getItems().byDefaultTimeout(), new Function<MultiSelect.Lozenge, String>() {
-//			@Override
-//			public String apply(@Nullable MultiSelect.Lozenge input) {
-			final Scriptable scope = cx.initStandardObjects();
-			final List<String> items = Lists.newArrayList();
-			scope.put("items", scope, items);
-			scope.put("select", scope, pageBinder.bind(getMultiSelectClass(pageBinder), id));
-
-			cx.evaluateString(scope, "for(var s = select.getItems().size(), i = 0; i<s; ++i) { items.add(select.getItems().get(i).getName()); }", "js", 1, null);
-
-			return items;
-		} finally {
-			cx.exit();
-		}
-	}
-
-	protected static <T> Class getMultiSelectClass(T owner) {
-		final Class select;
-		try {
-			select = owner.getClass().getClassLoader().loadClass("com.atlassian.jira.pageobjects.components.MultiSelect");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		return select;
+		return MultiSelectUtil.getMultiSelect(this.pageBinder, "groups");
 	}
 
 	public ConfigureCopyIssuesAdminActionPage submit() {
