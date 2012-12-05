527dd64c769d9858f23dec81310bb92eeb3ae56e
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-12-05 16:28:03 +0100

    JRADEV-16759: fixed poms so jersey-multipart is visible in tests

--- cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestCopyDetailsAction.scala
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestCopyDetailsAction.scala
@@ -7,7 +7,9 @@ import com.atlassian.jira.rest.client.domain.input.{ComplexIssueInputFieldValue,
 import com.atlassian.jira.rest.client.domain.IssueFieldId._
 import org.joda.time.DateTime
 import com.atlassian.cpji.tests.pageobjects.{CopyDetailsPage, SelectTargetProjectPage}
-import org.junit.Assert._;
+import org.junit.Assert._
+import java.io.ByteArrayInputStream
+;
 
 class TestCopyDetailsAction extends AbstractCopyIssueTest {
 	var createIssues: CreateIssues = new CreateIssues(AbstractCopyIssueTest.restClient1)
@@ -26,11 +28,14 @@ class TestCopyDetailsAction extends AbstractCopyIssueTest {
 		AbstractCopyIssueTest.restClient1.getIssueClient.addComment(AbstractCopyIssueTest.NPM, issue.getCommentsUri,
 			new Comment(null, "This is a comment", null, null, new DateTime, new DateTime, null, null))
 
+		AbstractCopyIssueTest.restClient1.getIssueClient.addAttachment(AbstractCopyIssueTest.NPM,
+			issue.getAttachmentsUri, new ByteArrayInputStream("this is a stream".getBytes("UTF-8")), this.getClass.getCanonicalName)
+
 		val selectTargetProjectPage: SelectTargetProjectPage = AbstractCopyIssueTest.jira1.visit(classOf[SelectTargetProjectPage], issue.getId)
 		val copyDetailsPage: CopyDetailsPage = selectTargetProjectPage.next()
 
 		assertTrue(copyDetailsPage.isCopyCommentsGroupVisible)
-		assertFalse(copyDetailsPage.isCopyAttachmentsGroupVisible)
+		assertTrue(copyDetailsPage.isCopyAttachmentsGroupVisible)
 		assertFalse(copyDetailsPage.isCopyIssueLinksGroupVisible)
 		assertTrue(copyDetailsPage.isCreateIssueLinksGroupVisible)
 	}
