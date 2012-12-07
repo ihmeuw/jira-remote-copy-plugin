0e96b52db1a7804528da1d827ec2f6850f1e2e06
Maciej Nowakowski <mnowakowski@atlassian.com>
2012-12-07 14:42:34 +0100

    NONE: unit tests - AbstractJsonResponseHandler, ProxyFactory

---
+++ cpji-jira-plugin-test/src/test/java/com/atlassian/cpji/components/remote/TestAbstractJsonResponseHandler.java
@@ -0,0 +1,117 @@
+package com.atlassian.cpji.components.remote;
+
+import com.atlassian.cpji.components.model.JiraLocation;
+import com.atlassian.cpji.components.model.NegativeResponseStatus;
+import com.atlassian.fugue.Either;
+import com.atlassian.sal.api.net.Response;
+import com.atlassian.sal.api.net.ResponseException;
+import org.codehaus.jettison.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @since v3.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestAbstractJsonResponseHandler {
+
+    private static class MyResponseType { }
+
+    private static class JsonResponseHandlerImpl extends AbstractJsonResponseHandler<MyResponseType>{
+
+        private final MyResponseType mockedResponse;
+        private final NegativeResponseStatus providedResponseStatus;
+
+        JsonResponseHandlerImpl(JiraLocation jiraLocation, MyResponseType mockedResponse, NegativeResponseStatus provideResponseStatus) {
+            super(jiraLocation);
+            this.mockedResponse = mockedResponse;
+            this.providedResponseStatus = provideResponseStatus;
+        }
+
+        @Override
+        protected MyResponseType parseResponse(Response response) throws ResponseException, JSONException {
+            if(providedResponseStatus != null){
+                provideResponseStatus(providedResponseStatus);
+            }
+            return mockedResponse;
+        }
+    }
+
+    private AbstractJsonResponseHandler<MyResponseType> jsonResponseHandler;
+
+    private final MyResponseType defaultResponse = new MyResponseType();
+    private final JiraLocation defaultJiraLocation = new JiraLocation("idIDid", "jiraName");
+
+    @Before
+    public void setUp(){
+        jsonResponseHandler = new JsonResponseHandlerImpl(defaultJiraLocation, defaultResponse, null);
+    }
+
+    @Test
+    public void testAuthenticationFailedOn401Response() throws Exception {
+        testStandardErrorResponse(401, NegativeResponseStatus.Status.AUTHENTICATION_FAILED);
+    }
+    @Test
+    public void testPluginNotInstalledOn404Response() throws Exception {
+        testStandardErrorResponse(404, NegativeResponseStatus.Status.PLUGIN_NOT_INSTALLED);
+    }
+
+    @Test
+    public void testCommunicationFailedOn500Response() throws Exception {
+        testStandardErrorResponse(500, NegativeResponseStatus.Status.COMMUNICATION_FAILED);
+    }
+
+    @Test
+    public void testReturnParsedOnSuccesfulResponse() throws Exception{
+        Response response = mockSuccesfulResponse(200);
+
+        Either<NegativeResponseStatus, MyResponseType> parsedResponse = jsonResponseHandler.handle(response);
+        assertTrue(parsedResponse.isRight());
+        assertEquals(defaultResponse, parsedResponse.right().get());
+    }
+
+    @Test
+    public void testReturnResponseStatusWhenProvided() throws Exception{
+        Response response = mockSuccesfulResponse(200);
+        JsonResponseHandlerImpl myHandler = new JsonResponseHandlerImpl(defaultJiraLocation, defaultResponse, NegativeResponseStatus.errorOccured(defaultJiraLocation, "singleError"));
+        Either<NegativeResponseStatus, MyResponseType> parsedResponse = myHandler.handle(response);
+        testNegativeResponse(NegativeResponseStatus.Status.ERROR_OCCURRED, parsedResponse);
+    }
+
+
+    private void testStandardErrorResponse(int responseCode, NegativeResponseStatus.Status expectedStatus) throws Exception {
+        Response response = mockUnsuccesfulResponse(responseCode);
+        Either<NegativeResponseStatus, MyResponseType> result = jsonResponseHandler.handle(response);
+        testNegativeResponse(expectedStatus, result);
+    }
+
+    private void testNegativeResponse(NegativeResponseStatus.Status expectedStatus, Either<NegativeResponseStatus, ?> givenResponse){
+        assertTrue(givenResponse.isLeft());
+        NegativeResponseStatus status = (NegativeResponseStatus) givenResponse.left().get();
+        assertEquals(expectedStatus, status.getResult());
+        assertEquals(defaultJiraLocation, status.getJiraLocation());
+    }
+
+    private Response mockUnsuccesfulResponse(int status){
+        Response response = mock(Response.class);
+        when(response.getStatusCode()).thenReturn(status);
+        when(response.isSuccessful()).thenReturn(false);
+        return response;
+    }
+
+    private Response mockSuccesfulResponse(int status){
+        Response response = mock(Response.class);
+        when(response.getStatusCode()).thenReturn(status);
+        when(response.isSuccessful()).thenReturn(true);
+        return response;
+    }
+
+
+}
