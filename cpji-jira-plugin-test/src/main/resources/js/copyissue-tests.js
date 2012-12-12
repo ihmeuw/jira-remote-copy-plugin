8cdfdb8c7566ab67b9b0742f4f3b435e630d3778
Maciej Nowakowski <mnowakowski@atlassian.com>
2012-12-12 09:45:23 +0100

    NONE: projects list response - qunit tests

--- cpji-jira-plugin-test/src/main/resources/js/copyissue-tests.js
+++ cpji-jira-plugin-test/src/main/resources/js/copyissue-tests.js
@@ -57,7 +57,7 @@ test("toggleLoadingState", function () {
 });
 
 
-test("onValueSelected/Unselected", function() {
+test("onValueSelected/Unselected", function () {
     var button = jQuery("<input type='Button' value='button'/>");
     this.fixture.append(button);
 
@@ -74,7 +74,7 @@ test("onValueSelected/Unselected", function() {
     equal("false", button.attr("aria-disabled"));
 });
 
-test("preparing select", function(){
+test("preparing select", function () {
     var select = jQuery("<div>fakeSelect</div>");
     sinon.stub(AJS, "SingleSelect");
     this.put.settings.projectsSelect = select;
@@ -90,4 +90,140 @@ test("preparing select", function(){
 
     AJS.SingleSelect.restore();
     select.bind.restore();
+});
+
+
+module("getProjectSuccess", {
+    setup: function () {
+        copyGeneralTools.init.call(this);
+        var container = jQuery("<div></div>")
+        this.put.settings.container = container;
+
+        this.prepareSingleServerResponse = function (appId) {
+            var result = {
+                id: appId,
+                projects: []
+            };
+            for (var i = 1; i < arguments.length; i += 2) {
+                result.projects.push({
+                    key: arguments[i],
+                    name: arguments[i + 1]
+                });
+            }
+            return result;
+        };
+
+        this.prepareResponseScheme = function () {
+            return {
+                projects: [],
+                failures: {}
+            };
+        };
+
+        this.successfulResponse = function () {
+            var scheme = this.prepareResponseScheme();
+            for (var i = 0; i < arguments.length; i++) {
+                scheme.projects.push(arguments[i]);
+            }
+            return scheme;
+        };
+
+
+    },
+    teardown: function () {
+    }
+});
+
+test("preparing projects list", function () {
+    this.put.settings.container.append("<div class='description'>This is some strange description</div>");
+
+    sinon.stub(this.put, "prepareSelect");
+    sinon.stub(this.put, "toggleLoadingState");
+    sinon.stub(this.put, "convertGroupToOptgroup");
+    this.put.convertGroupToOptgroup.returnsArg(0);
+
+    var select = jQuery("<div>mySelect</div>");
+    this.put.settings.projectsSelect = select;
+    sinon.stub(this.put.settings.projectsSelect, "append");
+
+
+    var localSrv = this.prepareSingleServerResponse("LOCAL", "TEST", "Testproj", "ABC", "Abc proj");
+    var remoteSrv = this.prepareSingleServerResponse("2w3459832", "TEST", "Testproj", "ABC", "Abc proj");
+    var response = this.successfulResponse(localSrv, remoteSrv);
+
+    this.put.getProjectsSuccess(response);
+
+    equal("", this.put.settings.container.html(), "Container shoud be empty");
+    ok(this.put.convertGroupToOptgroup.calledWith(localSrv), "Conversion was run");
+    ok(this.put.convertGroupToOptgroup.calledWith(remoteSrv));
+    ok(this.put.settings.projectsSelect.append.calledWith(localSrv), "Conversion result was appended to selection");
+    ok(this.put.settings.projectsSelect.append.calledWith(remoteSrv));
+    ok(this.put.toggleLoadingState.calledWith(false), "UI should leave loading state");
+    ok(this.put.prepareSelect.calledOnce, "Moder singleSelect should be prepared");
+
+
+    this.put.convertGroupToOptgroup.restore();
+    this.put.prepareSelect.restore();
+    this.put.toggleLoadingState.restore();
+    this.put.settings.projectsSelect.append.restore();
+});
+
+
+test("displaying error message on zero active projects", function () {
+    var response = this.successfulResponse();
+
+    var fakeLoader = jQuery("<div>loader</div>");
+    var fakeSelect = jQuery("<div>select</div>");
+
+    this.put.settings.loader = fakeLoader;
+    this.put.settings.projectsSelect = fakeSelect;
+
+    sinon.stub(RIC.Templates, "warningMsg");
+    RIC.Templates.warningMsg.returns("Rendered template");
+
+    sinon.stub(this.put, "onValueUnselected");
+
+
+    this.put.getProjectsSuccess(response);
+
+    equal("Rendered template", this.put.settings.container.html(), "Warning is appended to message");
+    ok(this.put.onValueUnselected.calledOnce, "Treat it as unselection");
+    ok(fakeLoader.hasClass("hidden"), "hide loader");
+    ok(fakeSelect.hasClass("hidden"), "hide singleSelect");
+
+
+    RIC.Templates.warningMsg.restore();
+    this.put.onValueUnselected.restore();
+
+});
+
+
+test("displaying failures", function () {
+    var response = this.successfulResponse(this.prepareSingleServerResponse("LOCAL", "KEY", "Project"));
+    response.failures.firstFailure = true;
+    response.failures.secondFailure = true;
+    sinon.stub(RIC.Templates, "remoteDestinationsNotAvailable");
+    RIC.Templates.remoteDestinationsNotAvailable.returns("Rendered template");
+
+
+    this.put.getProjectsSuccess(response);
+
+    equal('<div class="description">Rendered template</div>', this.put.settings.container.html());
+
+
+    RIC.Templates.remoteDestinationsNotAvailable.restore();
+});
+
+
+test("converting groups to optGroups and selecting default project", function () {
+    var localSrv = this.prepareSingleServerResponse("LOCAL", "KEY", "FirstProject", "OTH", "Other project");
+    var remoteSrv = this.prepareSingleServerResponse("REMOTE", "KEY", "FirstProject", "OTH", "Other project");
+    this.put.settings.defaultProject = "KEY";
+
+    var result = this.put.convertGroupToOptgroup(localSrv);
+    equal('<option value="LOCAL|KEY" selected="selected">FirstProject (KEY)</option><option value="LOCAL|OTH">Other project (OTH)</option>', result.html());
+
+
+    result = this.put.convertGroupToOptgroup(remoteSrv);
+    equal('<option value="REMOTE|KEY">FirstProject (KEY)</option><option value="REMOTE|OTH">Other project (OTH)</option>', result.html());
 });
--- cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/copyissue.js
+++ cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/copyissue.js
@@ -1,46 +1,49 @@
-AJS.$(function($){
+AJS.$(function ($) {
 
     var copyIssue = {
 
-        settings : {
-            contextPath : contextPath,
-            loader : null,
-            projectsSelect : null,
-            submitButton : null,
-            defaultInstance : "LOCAL",
-            defaultProject : null
+        settings: {
+            contextPath: contextPath,
+            loader: null,
+            projectsSelect: null,
+            submitButton: null,
+            defaultInstance: "LOCAL",
+            defaultProject: null
         },
 
-        singleSelect : null,
+        singleSelect: null,
 
-        initSelectProject : function(settings){
+        initSelectProject: function (settings) {
             $.extend(copyIssue.settings, settings);
             copyIssue.toggleLoadingState(true);
             copyIssue.getProjects();
         },
 
 
-        toggleLoadingState : function(visible){
+        toggleLoadingState: function (visible) {
             copyIssue.settings.loader.toggleClass("hidden", !visible);
             copyIssue.settings.projectsSelect.toggleClass("hidden", visible);
         },
 
-        getProjects : function(){
+        getProjects: function () {
             JIRA.SmartAjax.makeRequest({
-				url: (copyIssue.settings.contextPath + "/rest/copyissue/1.0/remotes/availableDestinations?issueId=" + AJS.$("input[name=id]").val()),
-				success: copyIssue.getProjectsSuccess
-			});
+                url: (copyIssue.settings.contextPath + "/rest/copyissue/1.0/remotes/availableDestinations?issueId=" + AJS.$("input[name=id]").val()),
+                success: copyIssue.getProjectsSuccess
+            });
         },
 
-        convertGroupToOptgroup : function(json) {
+        convertGroupToOptgroup: function (json) {
             var elem = $("<optgroup></optgroup>");
             elem.attr('label', json.name);
 
-            for(var i in json.projects) {
+            for (var i in json.projects) {
                 var project = json.projects[i];
                 var projElem = $("<option></option>");
-                if(json.id == copyIssue.settings.defaultInstance && project.key == copyIssue.settings.defaultProject){
-                    projElem.attr("selected", "selected");
+                if (json.id == copyIssue.settings.defaultInstance && project.key == copyIssue.settings.defaultProject) {
+
+                    //for some strange reason when I set this attribute throug explicit attr call sometimes it is not preserved
+                    projElem = $("<option selected='selected'></option>");
+//                    projElem.attr("selected", "selected");
                 }
                 projElem.attr('value', json.id + "|" + project.key);
                 projElem.text(project.name + " (" + project.key + ")");
@@ -49,35 +52,37 @@ AJS.$(function($){
             return elem;
         },
 
-        getProjectsSuccess : function(data) {
-			copyIssue.settings.container.find(".description").remove();
+        getProjectsSuccess: function (data) {
+            copyIssue.settings.container.find(".description").remove();
 
             if (data.projects) {
-                var projectCount = _.reduce(data.projects, function(sum, server){return sum + server.projects.length}, 0);
-                if(projectCount > 0){
-                    for(var server in data.projects) {
+                var projectCount = _.reduce(data.projects, function (sum, server) {
+                    return sum + server.projects.length
+                }, 0);
+                if (projectCount > 0) {
+                    for (var server in data.projects) {
                         var serverElem = copyIssue.convertGroupToOptgroup(data.projects[server]);
                         copyIssue.settings.projectsSelect.append(serverElem);
                     }
                     copyIssue.toggleLoadingState(false);
                     copyIssue.prepareSelect();
                 } else {
-                    copyIssue.settings.container.append(RIC.Templates.warningMsg({closeable: false, msg : AJS.I18n.getText("cpji.you.dont.have.create.issue.permission.for.any.project")}));
+                    copyIssue.settings.container.append(RIC.Templates.warningMsg({closeable: false, msg: AJS.I18n.getText("cpji.you.dont.have.create.issue.permission.for.any.project")}));
                     copyIssue.onValueUnselected();
                     copyIssue.settings.loader.toggleClass("hidden", true);
                     copyIssue.settings.projectsSelect.toggleClass("hidden", true);
                 }
-			}
+            }
 
-			if (data.failures && Object.keys(data.failures).length > 0) {
-				copyIssue.settings.container.append(AJS.$("<div class='description'></div>")
-						.append(RIC.Templates.remoteDestinationsNotAvailable(data.failures)));
-			}
+            if (data.failures && Object.keys(data.failures).length > 0) {
+                copyIssue.settings.container.append(AJS.$("<div class='description'></div>")
+                    .append(RIC.Templates.remoteDestinationsNotAvailable(data.failures)));
+            }
 
 
         },
 
-        prepareSelect : function() {
+        prepareSelect: function () {
             copyIssue.singleSelect = new AJS.SingleSelect({
                 element: copyIssue.settings.projectsSelect,
                 showDropdownButton: true,
@@ -87,21 +92,21 @@ AJS.$(function($){
             copyIssue.settings.projectsSelect.bind("unselect", copyIssue.onValueUnselected);
         },
 
-        onValueSelected : function (item) {
+        onValueSelected: function (item) {
             copyIssue.settings.submitButton.attr("disabled", false);
             copyIssue.settings.submitButton.attr("aria-disabled", false);
         },
 
-        onValueUnselected : function (item) {
+        onValueUnselected: function (item) {
             copyIssue.settings.submitButton.attr("disabled", true);
             copyIssue.settings.submitButton.attr("aria-disabled", true);
         }
 
     }
 
-    if(!JIRA.plugins){
+    if (!JIRA.plugins) {
         JIRA.plugins = {
-            cpjiPlugin : {}
+            cpjiPlugin: {}
         };
     } else {
         JIRA.plugins.cpjiPlugin = {};
@@ -111,10 +116,10 @@ AJS.$(function($){
 
 
     copyIssue.initSelectProject({
-		container: $("#targetEntityLink-container"),
+        container: $("#targetEntityLink-container"),
         projectsSelect: $("#targetEntityLink"),
-        loader : $("#targetEntityLinkLoader"),
-        submitButton : $("#select-project-submit"),
-        defaultProject : $("#cpji-current-project-key").text()
+        loader: $("#targetEntityLinkLoader"),
+        submitButton: $("#select-project-submit"),
+        defaultProject: $("#cpji-current-project-key").text()
     });
 });
