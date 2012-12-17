4b0a1e30a55e65fd40f6912935a243298eda6781
Maciej Nowakowski <mnowakowski@atlassian.com>
2012-12-17 15:25:06 +0100

    JRADEV-16761: hackish way of hiding default clone feature

--- cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/issuepage.js
+++ cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/issuepage.js
@@ -1,7 +1,43 @@
-AJS.$(function(){
-    var $clone = AJS.$("#clone-issue");
-    if($clone.length){
-        $clone.parent().remove();
-    }
-});
+(function ($) {
+
+    var cloneStyleClass = "issueaction-clone-issue";
+
+
+    var removeElements = function (context) {
+        var $clone = $("." + cloneStyleClass, context);
+        if ($clone.length) {
+            $clone.parent().remove();
+        }
+    };
+
+    $(document).ajaxSuccess(function (e, req, ajaxOptions) {
+        var operationsUrl = /.*\/rest\/api\/.*\/[0-9]+\/ActionsAndOperations/;
+        if (operationsUrl.test(ajaxOptions.url)) {
+            removeElements(e.target);
+        }
+    });
+
+    $(function () {
+        removeElements(document);
+        var formatResponse = JIRA.Dialogs.issueActions._formatActionsResponse;
+
+        JIRA.Dialogs.issueActions._formatActionsResponse = function (response) {
+            if (response && response.operations) {
+                response.operations = _.filter(response.operations, function (elem) {
+                    if (elem && elem.styleClass) {
+                        if (elem.styleClass == cloneStyleClass) {
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+            }
+            return formatResponse(response);
+        };
+
+    });
+
+
+})(AJS.$);
+
 
