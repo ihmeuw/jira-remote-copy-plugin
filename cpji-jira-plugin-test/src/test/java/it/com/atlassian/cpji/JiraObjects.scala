19d24b4ffa22cc376b9c219c4e43d98867b5e50d
Maciej Nowakowski <mnowakowski@atlassian.com>
2013-01-10 11:31:18 +0100

    NONE: visual consistency test

---
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/JiraObjects.scala
@@ -0,0 +1,17 @@
+package it.com.atlassian.cpji
+
+trait JiraObjects {
+	val jira1 = AbstractCopyIssueTest.jira1
+	val jira2 = AbstractCopyIssueTest.jira1
+	val jira3 = AbstractCopyIssueTest.jira1
+
+	val testkit1 = AbstractCopyIssueTest.testkit1
+	val testkit2 = AbstractCopyIssueTest.testkit2
+	val testkit3 = AbstractCopyIssueTest.testkit3
+
+	val restClient1 = AbstractCopyIssueTest.restClient1
+	val restClient2 = AbstractCopyIssueTest.restClient2
+	val restClient3 = AbstractCopyIssueTest.restClient3
+
+	val NPM = AbstractCopyIssueTest.NPM
+}
---
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestVisualConsistency.scala
@@ -0,0 +1,51 @@
+package it.com.atlassian.cpji
+
+import org.junit.{Rule, Test}
+import com.atlassian.cpji.tests.rules.CreateIssues
+import com.atlassian.jira.rest.client.domain.input.{LinkIssuesInput, ComplexIssueInputFieldValue, IssueInputBuilder}
+import com.atlassian.cpji.tests.pageobjects.SelectTargetProjectPage
+import com.atlassian.cpji.tests.ScreenshotUtil
+import java.io.ByteArrayInputStream
+import com.atlassian.jira.rest.client.domain.Comment
+import org.joda.time.DateTime
+
+class TestVisualConsistency extends AbstractCopyIssueTest with JiraObjects{
+
+	@Rule def createIssues = new CreateIssues(restClient1)
+
+	def takeScreenshot(message: String) {
+		ScreenshotUtil.attemptScreenshot(jira1.getTester.getDriver.getDriver, "VisualConsistency - "+message)
+	}
+
+	@Test def testTakeScreenshotsForEveryStep(){
+
+		testkit2.attachments().disable()
+
+		login(jira1)
+		val issueBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots").setAssigneeName("admin")
+		val issue = createIssues.newIssue(issueBuilder.build())
+
+		val issueToLinkBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots - I need one for linking")
+		val issueToLinkWith = createIssues.newIssue(issueToLinkBuilder.build())
+
+		restClient1.getIssueClient.addAttachment(NPM,
+			issue.getAttachmentsUri, new ByteArrayInputStream("this is a stream".getBytes("UTF-8")), this.getClass.getCanonicalName)
+
+		restClient1.getIssueClient.linkIssue(new LinkIssuesInput(issue.getKey, issueToLinkWith.getKey, "Duplicate"), NPM)
+
+		restClient1.getIssueClient.addComment(NPM, issue.getCommentsUri, new Comment(null, "Smile!", null, null, new DateTime, new DateTime, null, null))
+
+		val selectTargetProject = jira1.visit(classOf[SelectTargetProjectPage], issue.getId)
+		selectTargetProject.setDestinationProject("Destination not")
+		takeScreenshot("01 Select target project")
+		val details = selectTargetProject.next()
+		takeScreenshot("02 Select target project")
+		var confirm = details.next()
+		takeScreenshot("03 Confirm values")
+		confirm.copyIssue()
+		takeScreenshot("04 Successful screen")
+
+
+	}
+
+}
