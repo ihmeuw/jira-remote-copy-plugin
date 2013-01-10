f406fdd822c63f908143dfefe1b67fe9abe8eaf9
Maciej Nowakowski <mnowakowski@atlassian.com>
2013-01-10 12:44:42 +0100

    NONE: oops, enabling attachments after visual consistency test

--- cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestVisualConsistency.scala
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestVisualConsistency.scala
@@ -21,29 +21,34 @@ class TestVisualConsistency extends AbstractCopyIssueTest with JiraObjects{
 
 		testkit2.attachments().disable()
 
-		login(jira1)
-		val issueBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots").setAssigneeName("admin")
-		val issue = createIssues.newIssue(issueBuilder.build())
+		try{
 
-		val issueToLinkBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots - I need one for linking")
-		val issueToLinkWith = createIssues.newIssue(issueToLinkBuilder.build())
+			login(jira1)
+			val issueBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots").setAssigneeName("admin")
+			val issue = createIssues.newIssue(issueBuilder.build())
 
-		restClient1.getIssueClient.addAttachment(NPM,
-			issue.getAttachmentsUri, new ByteArrayInputStream("this is a stream".getBytes("UTF-8")), this.getClass.getCanonicalName)
+			val issueToLinkBuilder = new IssueInputBuilder("NEL", 3L, "Sample issue for screenshots - I need one for linking")
+			val issueToLinkWith = createIssues.newIssue(issueToLinkBuilder.build())
 
-		restClient1.getIssueClient.linkIssue(new LinkIssuesInput(issue.getKey, issueToLinkWith.getKey, "Duplicate"), NPM)
+			restClient1.getIssueClient.addAttachment(NPM,
+				issue.getAttachmentsUri, new ByteArrayInputStream("this is a stream".getBytes("UTF-8")), this.getClass.getCanonicalName)
 
-		restClient1.getIssueClient.addComment(NPM, issue.getCommentsUri, new Comment(null, "Smile!", null, null, new DateTime, new DateTime, null, null))
+			restClient1.getIssueClient.linkIssue(new LinkIssuesInput(issue.getKey, issueToLinkWith.getKey, "Duplicate"), NPM)
 
-		val selectTargetProject = jira1.visit(classOf[SelectTargetProjectPage], issue.getId)
-		selectTargetProject.setDestinationProject("Destination not")
-		takeScreenshot("01 Select target project")
-		val details = selectTargetProject.next()
-		takeScreenshot("02 Select target project")
-		var confirm = details.next()
-		takeScreenshot("03 Confirm values")
-		confirm.copyIssue()
-		takeScreenshot("04 Successful screen")
+			restClient1.getIssueClient.addComment(NPM, issue.getCommentsUri, new Comment(null, "Smile!", null, null, new DateTime, new DateTime, null, null))
+
+			val selectTargetProject = jira1.visit(classOf[SelectTargetProjectPage], issue.getId)
+			selectTargetProject.setDestinationProject("Destination not")
+			takeScreenshot("01 Select target project")
+			val details = selectTargetProject.next()
+			takeScreenshot("02 Select target project")
+			var confirm = details.next()
+			takeScreenshot("03 Confirm values")
+			confirm.copyIssue()
+			takeScreenshot("04 Successful screen")
+		} finally{
+			testkit2.attachments().enable()
+		}
 
 
 	}
