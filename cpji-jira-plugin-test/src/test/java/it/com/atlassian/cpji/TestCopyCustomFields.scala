6a2aeb77784f3e165173fb7981c6aa6c2b49004f
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2013-01-21 12:21:13 +0100

    JRADEV-18088: test copying issue local just to check sure if all built-in custom fields have mappers

--- cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestCopyCustomFields.scala
+++ cpji-jira-plugin-test/src/test/java/it/com/atlassian/cpji/TestCopyCustomFields.scala
@@ -1,10 +1,12 @@
 package it.com.atlassian.cpji
 
 import org.junit.{Test, Before}
-import org.junit.Assert.{assertTrue, assertEquals}
+import org.junit.Assert.{assertTrue, assertEquals, assertThat}
 import com.atlassian.cpji.tests.pageobjects.SelectTargetProjectPage
 import com.google.common.collect.Iterables
 import com.atlassian.jira.rest.client.domain.Field
+import collection.JavaConversions._
+import org.hamcrest.collection.IsIterableContainingInAnyOrder
 
 class TestCopyCustomFields extends AbstractCopyIssueTest with JiraObjects {
 	@Before def setup {
@@ -17,14 +19,20 @@ class TestCopyCustomFields extends AbstractCopyIssueTest with JiraObjects {
 		val copiedIssuePage = selectTargetPage.next().next().copyIssue()
 		assertTrue(copiedIssuePage.isSuccessful)
 
-		val i1 = restClient2.getIssueClient.getIssue("BLA-9", NPM)
+		val i1 = restClient2.getIssueClient.getIssue("ACF-1", NPM)
 		val i2 = restClient2.getIssueClient.getIssue(copiedIssuePage.getRemoteIssueKey, NPM)
 
 		val fields1 = List(Iterables.toArray(i1.getFields, classOf[Field]):_*)
 				.filter(p => p.getId.startsWith("customfield_") && p.getValue != null).map(f => (f.getName, f.getValue.toString)).sortBy(_._1)
 		val fields2 = List(Iterables.toArray(i2.getFields, classOf[Field]):_*)
 				.filter(p => p.getId.startsWith("customfield_") && p.getValue != null).map(f => (f.getName, f.getValue.toString)).sortBy(_._1)
-		assertEquals(21, fields1.length)
+		assertEquals(22, fields1.length)
 		assertEquals(fields2, fields1)
+
+		// should have at least those (may have more in the future)
+		assertThat(asJavaIterable(fields2.map(_._1)), IsIterableContainingInAnyOrder.containsInAnyOrder("Cascading Select",
+			"Custom labels", "Date Time", "DatePickerCF", "FreeTextFieldCF", "Group Picker", "GroupPickerCF", "Hidden Job Switch", "Job checkbox",
+			"Multi Select", "Multi User Picker", "Multi checkboxes", "MultiGroupPickerCF", "NumberFieldCF", "Project Picker",
+			"Radio Buttons", "SelectListCF", "Single Version Picker", "Text Field", "URL Field", "User Picker", "Version Picker"))
 	}
 }
