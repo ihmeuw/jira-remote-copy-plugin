6f27f2f9f60109cacc99feea9c49a4bd02e0f462
Maciej Nowakowski <mnowakowski@atlassian.com>
2013-04-05 12:52:20 +0200

    JRADEV-18299: hover information text

--- cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/copyIssueToInstance.js
+++ cpji-jira-plugin/src/main/resources/com/atlassian/cpji/javascript/copyIssueToInstance.js
@@ -1,16 +1,17 @@
 (function ($) {
     $(function () {
+        var isWarningAdded = false;
         $(".fields-list label").each(function(){
             var labelTarget = $(this).attr("for");
             var unmapped = $("#unmapped-for-"+labelTarget);
             if(unmapped.length == 1){
+                isWarningAdded = true;
                 var unmappedJson = JSON.parse(unmapped.text());
                 if(unmappedJson.length > 0){
                     var unmappedMarker = $(RIC.Templates.warningMarker());
                     $(this).prepend(unmappedMarker);
                     AJS.InlineDialog(unmappedMarker, "unmapped-dialog-"+labelTarget,
                         function (content, trigger, showPopup) {
-
                             content.html(RIC.Templates.unmappedValues({
                                 unmappedValues : unmappedJson
                             }));
@@ -21,6 +22,10 @@
                 }
             }
         });
+        console.log(isWarningAdded);
+        if(isWarningAdded){
+            $(".hover-over-info").removeClass("hidden");
+        }
 
     });
 })(AJS.$);
