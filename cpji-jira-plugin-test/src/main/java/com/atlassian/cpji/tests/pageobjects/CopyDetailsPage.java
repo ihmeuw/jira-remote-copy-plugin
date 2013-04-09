60b0073b62c4b0acb128167427552f3f8bc9412c
Maciej Nowakowski <mnowakowski@atlassian.com>
2013-04-09 12:08:48 +0200

    JRADEV-17778: testing both forms of attachment's notice info

--- cpji-jira-plugin-test/src/main/java/com/atlassian/cpji/tests/pageobjects/CopyDetailsPage.java
+++ cpji-jira-plugin-test/src/main/java/com/atlassian/cpji/tests/pageobjects/CopyDetailsPage.java
@@ -8,6 +8,7 @@ import com.atlassian.pageobjects.elements.ElementBy;
 import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.query.Conditions;
 import com.atlassian.pageobjects.elements.query.TimedCondition;
+import com.atlassian.pageobjects.elements.query.TimedQuery;
 import org.hamcrest.Matchers;
 import org.openqa.selenium.By;
 
@@ -18,8 +19,7 @@ import java.util.List;
  *
  * @since v2.1
  */
-public class CopyDetailsPage extends AbstractJiraPage
-{
+public class CopyDetailsPage extends AbstractJiraPage {
     private static final String URI_TEMPLATE = "/secure/CopyDetailsAction.jspa?id=%d&targetEntityLink=%s";
 
     private final Long issueId;
@@ -28,96 +28,92 @@ public class CopyDetailsPage extends AbstractJiraPage
     @ElementBy(className = "submit")
     PageElement nextButton;
 
-	@ElementBy(id = "create-issue-links-group")
-	PageElement createIssueLinkGroup;
+    @ElementBy(id = "create-issue-links-group")
+    PageElement createIssueLinkGroup;
 
-	@ElementBy (id = "copy-comments")
-	CheckboxElement copyComments;
+    @ElementBy(id = "copy-comments")
+    CheckboxElement copyComments;
 
-	@ElementBy (id = "copy-comments-group")
-	PageElement copyCommentsGroup;
+    @ElementBy(id = "copy-comments-group")
+    PageElement copyCommentsGroup;
 
-	@ElementBy (tagName = "div", within = "copyCommentsGroup")
-	PageElement copyCommentsNotice;
+    @ElementBy(tagName = "div", within = "copyCommentsGroup")
+    PageElement copyCommentsNotice;
 
-	@ElementBy (id = "copy-issue-links")
-	CheckboxElement copyIssueLinks;
+    @ElementBy(id = "copy-issue-links")
+    CheckboxElement copyIssueLinks;
 
-	@ElementBy (id = "copy-issue-links-group")
-	PageElement copyIssueLinksGroup;
+    @ElementBy(id = "copy-issue-links-group")
+    PageElement copyIssueLinksGroup;
 
-	@ElementBy (tagName = "div", within = "copyIssueLinksGroup")
-	PageElement copyIssueLinksNotice;
+    @ElementBy(tagName = "div", within = "copyIssueLinksGroup")
+    PageElement copyIssueLinksNotice;
 
-	@ElementBy (id = "copy-attachments")
-	CheckboxElement copyAttachments;
+    @ElementBy(id = "copy-attachments")
+    CheckboxElement copyAttachments;
 
-	@ElementBy (id = "copy-attachments-group")
-	PageElement copyAttachmentsGroup;
+    @ElementBy(id = "copy-attachments-group")
+    PageElement copyAttachmentsGroup;
 
-	@ElementBy (tagName = "div", within = "copyAttachmentsGroup")
-	PageElement copyAttachmentsNotice;
+    @ElementBy(tagName = "div", within = "copyAttachmentsGroup")
+    PageElement copyAttachmentsNotice;
 
-    @ElementBy (id = "summary")
+    @ElementBy(id = "summary")
     PageElement summary;
 
     SingleSelect remoteIssueLink;
 
-    public CopyDetailsPage(final Long issueId, final String targetEntityLink)
-    {
+    public CopyDetailsPage(final Long issueId, final String targetEntityLink) {
         this.issueId = issueId;
         this.url = String.format(URI_TEMPLATE, issueId, targetEntityLink);
     }
 
     @Init
-    public void init(){
+    public void init() {
         final PageElement remoteIssueLinkContainer = elementFinder.find(By.id("remoteIssueLink-single-select"));
         remoteIssueLink = pageBinder.bind(SingleSelect.class, remoteIssueLinkContainer);
     }
 
     @Override
-    public TimedCondition isAt()
-    {
+    public TimedCondition isAt() {
         return Conditions.forMatcher(elementFinder.find(By.className("current")).timed().getText(), Matchers.equalToIgnoringCase("Details"));
     }
 
     @Override
-    public String getUrl()
-    {
+    public String getUrl() {
         return url;
     }
 
-    public CopyIssueToInstanceConfirmationPage next()
-    {
+    public CopyIssueToInstanceConfirmationPage next() {
         nextButton.click();
         return pageBinder.bind(CopyIssueToInstanceConfirmationPage.class);
     }
 
-	public boolean isCreateIssueLinksGroupVisible() {
-		return createIssueLinkGroup.timed().isVisible().now();
-	}
+    public boolean isCreateIssueLinksGroupVisible() {
+        return createIssueLinkGroup.timed().isVisible().now();
+    }
 
-	public PageElement getCopyCommentsGroup() {
-		return copyCommentsGroup;
-	}
+    public PageElement getCopyCommentsGroup() {
+        return copyCommentsGroup;
+    }
 
-	public PageElement getCopyIssueLinksGroup() {
-		return copyIssueLinksGroup;
-	}
+    public PageElement getCopyIssueLinksGroup() {
+        return copyIssueLinksGroup;
+    }
 
-	public PageElement getCopyAttachmentsGroup() {
-		return copyAttachmentsGroup;
-	}
+    public PageElement getCopyAttachmentsGroup() {
+        return copyAttachmentsGroup;
+    }
 
 
-	public List<String> getCreateIssueLinks() {
+    public List<String> getCreateIssueLinks() {
         remoteIssueLink.clear().triggerSuggestions();
         return remoteIssueLink.getSuggestions();
-	}
+    }
 
-    public CopyDetailsPage setCreateIssueLink(RemoteIssueLinkType value){
+    public CopyDetailsPage setCreateIssueLink(RemoteIssueLinkType value) {
         final String selectedValue;
-        switch(value){
+        switch (value) {
             case RECIPROCAL:
                 selectedValue = "Both";
                 break;
@@ -138,31 +134,41 @@ public class CopyDetailsPage extends AbstractJiraPage
         return this;
     }
 
-	public CheckboxElement getCopyIssueLinks() {
-		return copyIssueLinks;
-	}
+    public CheckboxElement getCopyIssueLinks() {
+        return copyIssueLinks;
+    }
 
-	public CheckboxElement getCopyAttachments() {
-		return copyAttachments;
-	}
+    public CheckboxElement getCopyAttachments() {
+        return copyAttachments;
+    }
 
-	public CheckboxElement getCopyComments() {
-		return copyComments;
-	}
+    public CheckboxElement getCopyComments() {
+        return copyComments;
+    }
 
-	public PageElement getCopyCommentsNotice() {
-		return copyCommentsNotice;
-	}
+    public PageElement getCopyCommentsNotice() {
+        return copyCommentsNotice;
+    }
 
-	public PageElement getCopyIssueLinksNotice() {
-		return copyIssueLinksNotice;
-	}
+    public PageElement getCopyIssueLinksNotice() {
+        return copyIssueLinksNotice;
+    }
 
-	public PageElement getCopyAttachmentsNotice() {
-		return copyAttachmentsNotice;
-	}
+    public PageElement getCopyAttachmentsNotice() {
+        return copyAttachmentsNotice;
+    }
 
-    public CopyDetailsPage enterNewSummary(String summary){
+    public void showNotCopiedAttachmentsList() {
+        PageElement lozenge = copyAttachmentsNotice.find(By.className("aui-lozenge"));
+        lozenge.click();
+    }
+
+
+    public TimedQuery<String> getNotCopiedAttachmentsList() {
+        return elementFinder.find(By.cssSelector("#inline-dialog-attachments-popup ul")).timed().getText();
+    }
+
+    public CopyDetailsPage enterNewSummary(String summary) {
         this.summary.clear().type(summary);
         return this;
     }
